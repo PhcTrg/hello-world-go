c13c8eced4a73e5c791c430a412d8c4c7b92efe8
Phước Trường <152671615+PhcTrg@users.noreply.github.com>
2024-09-19 22:13:12 +0700

    update

--- hello-world.go
+++ hello-world.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Build from AWS");
-	fmt.Println("Hello world");
+	fmt.Println("Build from commit: $CODEBUILD_RESOLVED_SOURCE_VERSION")
+	fmt.Println("Hello world")
 }
